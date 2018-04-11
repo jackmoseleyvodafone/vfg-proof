93809cfd01dd6bfe2a99bed16bd6c9fcf8ba26b2
vodafone <vodafone@vodafones-MacBook-Air.local>
2018-04-11 14:29:37 +0100

    first commit

---
+++ VFGProof/ViewControllers/Base/Views/BaseView.swift
@@ -0,0 +1,21 @@
+//
+//  BaseView.swift
+//  VFGProof
+//
+//  Created by vodafone on 03/04/2018.
+//  Copyright © 2018 vodafone. All rights reserved.
+//
+
+import UIKit
+
+class BaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
