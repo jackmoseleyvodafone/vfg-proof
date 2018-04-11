b7cae89a397bf8065a47de8e173724fb309f2314
vodafone <vodafone@vodafones-MacBook-Air.local>
2018-04-11 14:36:23 +0100

    Removing any reference to alternative splash loading method

--- VFGProof/ViewControllers/Main/MainViewController.swift
+++ VFGProof/ViewControllers/Main/MainViewController.swift
@@ -13,33 +13,5 @@ class MainViewController: BaseViewController {
     
     override func loadView() {
         super.loadView()
-        
-//        loadProposedSolution();
-        
-//        loadUndocumentedSolution();
-    }
-    
-    private func loadProposedSolution() {
-//        self.view.addSubview(VFGAnimatedSplash.sharedInstance.returnViewController().view)
-//
-//            VFGAnimatedSplash.sharedInstance.startSplashAnimation(completion: {
-//            VFGAnimatedSplash.sharedInstance.returnViewController().view.removeFromSuperview()
-//
-//            // set main root view to next screen
-//            self.view = rootView
-//        })
-    }
-    
-    private func loadUndocumentedSolution() {
-        
-        VFGAnimatedSplash.sharedInstance.setComplitionHandler { (result) in
-            VFGAnimatedSplash.sharedInstance.returnViewController().view.removeFromSuperview()
-        }
-
-        self.view.addSubview(VFGAnimatedSplash.sharedInstance.returnViewController().view)
-        
-        VFGAnimatedSplash.sharedInstance.splashInstance = VFGAnimatedSplash.sharedInstance.returnViewController()
-
-        VFGAnimatedSplash.sharedInstance.runSplashAnimationFor(timeOut: 2)
     }
 }
